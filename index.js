4472b74edeb32ef5b57fd625cdaac843aa4c6fb0
Tareq Al-Zubaidi <Tareq.Al-Zubaidi@matchesfashion.com>
2019-12-19 20:34:37 +0000

    Update greeting msg

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const responseString = `<h2 style="text-align:center">Hello World!</h2>
+  <h4 style="text-align:center">Message received: "${process.env.PODINFO_UI_MESSAGE}"</h4>`;
 
 app.get('/healthz', (req, res) => {
   res.json({ status: 'OK!' });
@@ -12,7 +14,7 @@ app.get('/readyz', (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-  res.send(`<h2>Hello World!</h2><h4>We got "${process.env.PODINFO_UI_MESSAGE}"</h4>`);
+  res.send(responseString);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
